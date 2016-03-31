27bcd71089201f021dcbaeb40229c0dfae9cc3ed
guenneguezt <thomas@guenneguez.com>
2016-03-31 12:42:29 +0200

    Version 1.2

---
+++ core/class/qualiteair.class.php
@@ -0,0 +1,238 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+
+class qualiteair extends eqLogic {
+    /*     * *************************Attributs****************************** */
+
+
+    /*     * ***********************Methode static*************************** */
+
+	public static function ListVille() {
+		$status1 = $status2 = array();
+		$data = simplexml_load_file("http://www.lcsqa.org/surveillance/indices/prevus/jour/xml/".date('Y-m-d', time() - 24 * 60 * 60));
+		if ( $data !== false )
+		{
+			$xpathModele = '//agglomeration';
+			$status1 = $data->xpath($xpathModele);
+		}
+		$data = simplexml_load_file("http://www.lcsqa.org/surveillance/indices/prevus/jour/xml/".date('Y-m-d', time()));
+		if ( $data !== false )
+		{
+			$xpathModele = '//agglomeration';
+			$status2 = $data->xpath($xpathModele);
+		}
+		$status =  array_merge ($status1, $status2);
+		if ( count($status) != 0 )
+		{
+			sort($status, SORT_STRING);
+			return $status;
+		}
+		else
+		{
+			return array();
+		}
+	}
+
+	public static function cronHourly() {
+		foreach (self::byType('qualiteair') as $eqLogic) {
+			$eqLogic->pull();
+		}
+	}
+
+	public function postUpdate()
+	{
+		$cmd = $this->getCmd(null, 'status');
+		if ( ! is_object($cmd) ) {
+			$cmd = new qualiteairCmd();
+			$cmd->setName('Etat');
+			$cmd->setEqLogic_id($this->getId());
+			$cmd->setType('info');
+			$cmd->setSubType('binary');
+			$cmd->setLogicalId('status');
+			$cmd->setIsVisible(1);
+			$cmd->setEventOnly(1);
+			$cmd->save();
+		}
+		$cmd = $this->getCmd(null, 'valeurIndice');
+		if ( ! is_object($cmd) ) {
+			$cmd = new qualiteairCmd();
+			$cmd->setName('Aujourd\'hui');
+			$cmd->setEqLogic_id($this->getId());
+			$cmd->setType('info');
+			$cmd->setSubType('numeric');
+			$cmd->setLogicalId('valeurIndice');
+			$cmd->setIsVisible(1);
+			$cmd->setEventOnly(1);
+			$cmd->setTemplate('dashboard', 'qualiteair');
+			$cmd->setTemplate('mobile', 'qualiteair');
+			$cmd->save();
+		}
+		$cmd = $this->getCmd(null, 'valeurIndiceDemain');
+		if ( ! is_object($cmd) ) {
+			$cmd = new qualiteairCmd();
+			$cmd->setName('Demain');
+			$cmd->setEqLogic_id($this->getId());
+			$cmd->setType('info');
+			$cmd->setSubType('numeric');
+			$cmd->setLogicalId('valeurIndiceDemain');
+			$cmd->setIsVisible(1);
+			$cmd->setEventOnly(1);
+			$cmd->setTemplate('dashboard', 'qualiteair');
+			$cmd->setTemplate('mobile', 'qualiteair');
+			$cmd->save();
+		}
+		$cmd = $this->getCmd(null, 'SousIndiceO3');
+		if ( ! is_object($cmd) ) {
+			$cmd = new qualiteairCmd();
+			$cmd->setName('O3');
+			$cmd->setEqLogic_id($this->getId());
+			$cmd->setType('info');
+			$cmd->setSubType('numeric');
+			$cmd->setLogicalId('SousIndiceO3');
+			$cmd->setIsVisible(1);
+			$cmd->setEventOnly(1);
+			$cmd->setTemplate('dashboard', 'qualiteair');
+			$cmd->setTemplate('mobile', 'qualiteair');
+			$cmd->save();
+		}
+		$cmd = $this->getCmd(null, 'SousIndiceNO2');
+		if ( ! is_object($cmd) ) {
+			$cmd = new qualiteairCmd();
+			$cmd->setName('NO2');
+			$cmd->setEqLogic_id($this->getId());
+			$cmd->setType('info');
+			$cmd->setSubType('numeric');
+			$cmd->setLogicalId('SousIndiceNO2');
+			$cmd->setIsVisible(1);
+			$cmd->setEventOnly(1);
+			$cmd->setTemplate('dashboard', 'qualiteair');
+			$cmd->setTemplate('mobile', 'qualiteair');
+			$cmd->save();
+		}
+		$cmd = $this->getCmd(null, 'SousIndicePM10');
+		if ( ! is_object($cmd) ) {
+			$cmd = new qualiteairCmd();
+			$cmd->setName('PM10');
+			$cmd->setEqLogic_id($this->getId());
+			$cmd->setType('info');
+			$cmd->setSubType('numeric');
+			$cmd->setLogicalId('SousIndicePM10');
+			$cmd->setIsVisible(1);
+			$cmd->setEventOnly(1);
+			$cmd->setTemplate('dashboard', 'qualiteair');
+			$cmd->setTemplate('mobile', 'qualiteair');
+			$cmd->save();
+		}
+		$cmd = $this->getCmd(null, 'SousIndiceSO2');
+		if ( ! is_object($cmd) ) {
+			$cmd = new qualiteairCmd();
+			$cmd->setName('S02');
+			$cmd->setEqLogic_id($this->getId());
+			$cmd->setType('info');
+			$cmd->setSubType('numeric');
+			$cmd->setLogicalId('SousIndiceSO2');
+			$cmd->setIsVisible(1);
+			$cmd->setEventOnly(1);
+			$cmd->setTemplate('dashboard', 'qualiteair');
+			$cmd->setTemplate('mobile', 'qualiteair');
+			$cmd->save();
+		}
+		$cmd = $this->getCmd(null, 'Commentaire');
+		if ( ! is_object($cmd) ) {
+			$cmd = new qualiteairCmd();
+			$cmd->setName('Commentaire');
+			$cmd->setEqLogic_id($this->getId());
+			$cmd->setType('info');
+			$cmd->setSubType('string');
+			$cmd->setLogicalId('Commentaire');
+			$cmd->setIsVisible(1);
+			$cmd->setEventOnly(1);
+			$cmd->save();
+		}
+		$this->pull();
+	}
+
+	public function pull() {
+		if ( $this->getIsEnable() ) {
+			log::add('qualiteair','debug','pull '.$this->getName());
+			$statuscmd = $this->getCmd(null, 'status');
+			$data = simplexml_load_file("http://www.lcsqa.org/surveillance/indices/prevus/jour/xml/".date('Y-m-d'));
+			$count = 0;
+			while ( $data === false && $count < 3 ) {
+				$data = simplexml_load_file("http://www.lcsqa.org/surveillance/indices/prevus/jour/xml/".date('Y-m-d'));
+				$count++;
+			}
+			if ( $data === false ) {
+				if ($statuscmd->execCmd() != 0) {
+					$statuscmd->setCollectDate('');
+					$statuscmd->event(0);
+				}
+				log::add('qualiteair','error',__('Impossible de se connecter à www.lcsqa.org.',__FILE__));
+				return false;
+			}
+			$xpathModele = '//agglomeration[. ="'.$this->getConfiguration('ville').'"]/parent::*';
+			$status = $data->xpath($xpathModele);
+			if ( count($status) != 0 ) {
+				foreach ($status[0] as $key => $value) {
+					$eqLogic_cmd = $this->getCmd(null, $key);
+					if ( is_object($eqLogic_cmd) && $eqLogic_cmd->execCmd(null, 2) != $eqLogic_cmd->formatValue($value)) {
+						log::add('qualiteair','debug',"Change ".$eqLogic_cmd->getName());
+						$eqLogic_cmd->setCollectDate('');
+						$eqLogic_cmd->event($value);
+					}
+				}				
+				$statuscmd->setCollectDate('');
+				$statuscmd->event(1);
+			}
+			else {
+				$statuscmd->setCollectDate('');
+				$statuscmd->event(0);
+			}
+			$data = simplexml_load_file("http://www.lcsqa.org/surveillance/indices/prevus/jour/xml/".date('Y-m-d', time() + 24 * 60 * 60));
+			$count = 0;
+			while ( $data === false && $count < 3 ) {
+				$data = simplexml_load_file("http://www.lcsqa.org/surveillance/indices/prevus/jour/xml/".date('Y-m-d', time() + 24 * 60 * 60));
+				$count++;
+			}
+			$xpathModele = '//agglomeration[. ="'.$this->getConfiguration('ville').'"]/parent::*';
+			$status = $data->xpath($xpathModele);
+			if ( count($status) != 0 ) {
+				foreach ($status[0] as $key => $value) {
+					$eqLogic_cmd = $this->getCmd(null, $key);
+					if ( is_object($eqLogic_cmd) && $eqLogic_cmd->execCmd(null, 2) != $eqLogic_cmd->formatValue($value)) {
+						log::add('qualiteair','debug',"Change ".$eqLogic_cmd->getName());
+						$eqLogic_cmd->setCollectDate('');
+						$eqLogic_cmd->event($value);
+					}
+				}
+			}
+			log::add('qualiteair','debug','pull end '.$this->getName());
+		}
+	}
+
+    /*     * *********************Methode d'instance************************* */
+}
+
+class qualiteairCmd extends cmd {
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+?>
---
+++ desktop/php/qualiteair.php
@@ -0,0 +1,120 @@
+<?php
+if (!isConnect('admin')) {
+    throw new Exception('{{401 - Accès non autorisé}}');
+}
+sendVarToJS('eqType', 'qualiteair');
+$eqLogics = eqLogic::byType('qualiteair');
+?>
+
+<div class="row row-overflow">
+    <div class="col-lg-2 col-md-3 col-sm-4">
+        <div class="bs-sidebar">
+            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="icon divers-triangular42"></i> {{Ajouter une ville}}</a>
+                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+                <?php
+                foreach ($eqLogics as $eqLogic) {
+                    echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+    
+     <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+        <legend>{{Mes villes}}
+        </legend>
+		<div class="eqLogicThumbnailContainer">
+		  <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+			 <center>
+				<i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+			</center>
+			<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+			</div>
+			<?php
+			if (count($eqLogics) == 0) {
+				echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>Vous n'avez encore de ville de configuré, cliquez à gauche sur le bouton ajouter une ville pour commencer</span></center>";
+			} else {
+                foreach ($eqLogics as $eqLogic) {
+                    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+                    echo "<center>";
+                    echo '<img src="plugins/qualiteair/doc/images/qualiteair_icon.jpg" height="105" width="95" />';
+                    echo "</center>";
+                    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+                    echo '</div>';
+                }
+			} ?>
+		</div>
+    </div>
+
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+        <form class="form-horizontal">
+            <fieldset>
+                <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">{{Nom de l'équipement}}</label>
+                    <div class="col-sm-2">
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="Nom de l'équipement"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label" >{{Objet parent}}</label>
+                    <div class="col-sm-2">
+                        <select class="eqLogicAttr form-control" data-l1key="object_id">
+                            <option value="">{{Aucun}}</option>
+                            <?php
+                            foreach (object::all() as $object) {
+                                echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-sm-2 control-label" ></label>
+					<div class="col-sm-10">
+					<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
+					<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+					</div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-2 control-label">{{Ville}}</label>
+                    <div class="col-lg-3">
+                        <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="ville">
+							<?php
+							foreach ( qualiteair::ListVille() as $Ville) {
+								echo '<option value="' . $Ville . '">' . ucwords(strtolower($Ville)) . '</option>';
+							}
+							?>
+                        </select>
+                    </div>
+                </div>
+            </fieldset> 
+        </form>
+
+        <legend>{{Mes indicateurs}}</legend>
+        <table id="table_cmd" class="table table-bordered table-condensed">
+            <thead>
+                <tr>
+                    <th>{{Id}}</th><th>{{Nom}}</th><th>{{Valeur par défaut}}</th><th></th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+
+        <form class="form-horizontal">
+            <fieldset>
+                <div class="form-actions">
+                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+</div>
+
+<?php include_file('desktop', 'qualiteair', 'js', 'qualiteair'); ?>
+<?php include_file('core', 'plugin.template', 'js'); ?>
