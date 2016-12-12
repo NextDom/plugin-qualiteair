15465cb08411544ef702b3b7be3964622382109e
guenneguezt <thomas@guenneguez.com>
2016-12-12 22:34:15 +0100

    Correction bouton visible et actif

--- desktop/php/qualiteair.php
+++ desktop/php/qualiteair.php
@@ -89,8 +89,8 @@ $eqLogics = eqLogic::byType('qualiteair');
                 <div class="form-group">
                   <label class="col-sm-2 control-label" ></label>
 					<div class="col-sm-10">
-					<input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
-					<input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+					<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>Activer</label>
+					<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>Visible</label>
 					</div>
                 </div>
                 <div class="form-group">
